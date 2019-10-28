b3c4b69a20aabdf8cd3c3e00217da389f2d1eec3
Brian Le <brainicism@gmail.com>
2019-10-27 18:59:50 -0700

    Add unique constraint on discord_id, server_id in User table

--- models/users.py
+++ models/users.py
@@ -4,3 +4,6 @@ from .base_model import BaseModel
 class Users(BaseModel):
     discord_id = CharField()
     server_id = CharField()
+
+    class Meta:
+        constraints = [SQL("UNIQUE(discord_id, server_id)")]
