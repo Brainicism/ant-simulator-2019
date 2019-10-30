3dc5a7abbcb80904f084668b55322fb4a0df8122
Brian Le <brainicism@gmail.com>
2019-10-29 21:43:27 -0700

    Personalize seed data with discord_id and server_id

--- seed_test_data.py
+++ seed_test_data.py
@@ -6,15 +6,21 @@ from models.species import Species
 from models.forage_events import ForageEvents
 db = SqliteDatabase('main.db')
 db.connect()
+import configparser
 db.create_tables([Ants, Colony, Species, ForageEvents, Users])
+config = configparser.ConfigParser()
+config.read("config.ini")
 
-for discord_id in range(0, 5):
-    user_id = Users.insert(discord_id=discord_id, server_id=100).execute()
-    colony_id = Colony.insert(
-        user_id=user_id,
-        species_id=0,
-        colony_name=f"Epic Colony {discord_id}"
-    ).execute()
+if not "seed" in config:
+    raise Exception("Missing seed data in config")
+discord_id = config["seed"]["UserId"]
+server_id = config["seed"]["ServerId"]
 
-    test_ants = [{"colony_id": colony_id, "name": f"ant-{str(x)}", "role": 'worker', "life_stage": 3 } for x in range(0,10)]
-    Ants.insert_many(test_ants).execute()
+user_id = Users.insert(discord_id=discord_id, server_id=server_id).execute()
+colony_id = Colony.insert(
+    user_id=user_id,
+    species_id=0,
+    colony_name=f"Epic Colony {discord_id}"
+).execute()
+test_ants = [{"colony_id": colony_id, "name": f"ant-{str(x)}", "role": 'worker', "life_stage": 3 } for x in range(0,10)]
+Ants.insert_many(test_ants).execute()
