627985c2a86fc6912069c0ae3894ca75ab92c67c
Brian Le <brainicism@gmail.com>
2019-10-29 22:12:30 -0700

    Implement dummy ant species + seed + join

--- cogs/join.py
+++ cogs/join.py
@@ -1,10 +1,12 @@
 import random
-
+import discord
 from peewee import *
 from models.ants import Ants
 from models.colony import Colony
 from models.users import Users
+from models.species import Species
 from discord.ext import commands
+
 
 class Join(commands.Cog):
     def __init__(self, client):
@@ -12,13 +14,13 @@ class Join(commands.Cog):
 
     @commands.command()
     async def join(self, ctx):
-        species = ['Camponotus', 'Yellow Crazy Ants']
+        species = Species.select().order_by(fn.Random()).first()
         message = ctx.message
         if not Users.select().where((Users.discord_id == message.author.id) & (Users.server_id == message.guild.id)):
             user_id = Users.insert(discord_id=str(message.author.id), server_id=str(message.guild.id)).execute()
             colony_id = Colony.insert(
                 user_id=user_id,
-                species_id=random.choice(species),
+                species_id=species.id,
                 colony_name=message.author.name + '\'s Colony'
             ).execute()
             Ants.insert(
@@ -28,5 +30,8 @@ class Join(commands.Cog):
                 life_stage=3
             ).execute()
             await message.channel.send('You have joined the game!')
+            embed = discord.Embed(title=f"You have selected {species.species_name}", description=f"HP: {species.hp_multiplier} Forage: {species.forage_multiplier}", color=0x00ff00)
+            embed.set_image(url = species.image_url)
+            await message.channel.send(embed=embed)
         else:
             await message.channel.send('You have already joined this game.')
