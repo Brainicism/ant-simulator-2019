9613c2b8e6ddbb279dc91d77221f24b033b044b9
Brian Le <brainicism@gmail.com>
2019-10-30 22:39:56 -0700

    Add event loop, check for eggs ready to hatch

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
-import discord
 import configparser
+import importlib.util
+import os
+import os.path
+import threading
+import time
+from os.path import basename, dirname
+
+import discord
+import schedule
+from discord.ext import commands
+from peewee import *
+
+import seed
+from cogs.birth import Birth
+from cogs.join import Join
+from cogs.leave import Leave
+from cogs.ping import Ping
+from event_loop import trigger
 from models.ants import Ants
 from models.colony import Colony
 from models.forage_events import ForageEvents
 from models.species import Species
 from models.users import Users
-from discord.ext import commands
-import seed
-from peewee import *
-from os.path import dirname, basename
-import importlib.util
-import os
-from cogs.join import Join
-from cogs.leave import Leave
-from cogs.birth import Birth
-from cogs.ping import Ping
 
 print("Reading config...")
 config = configparser.ConfigParser()
@@ -28,7 +35,6 @@ bot.add_cog(Ping(bot))
 
 print("Starting database...")
 
-import os.path
 if not os.path.isfile("main.db"):
     seed.seed()
 db = SqliteDatabase('main.db')
@@ -39,4 +45,12 @@ async def on_ready():
     print(bot.user.name)
     print(bot.user.id)
 
+
+def run_event_loop():
+    schedule.every(10).minutes.do(trigger)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+t = threading.Thread(target=run_event_loop).start()
 bot.run(config["discordbot"]["Token"])
--- models/ants.py
+++ models/ants.py
@@ -4,7 +4,7 @@ from .colony import Colony
 from .base_model import BaseModel
 
 class Ants(BaseModel):
-    colony_id = ForeignKeyField(Colony, to_field="id", on_delete="CASCADE")
+    colony_id = ForeignKeyField(Colony, to_field="id", on_delete="CASCADE", backref="ants")
     name = CharField()
     role = CharField()
     life_stage = IntegerField()
