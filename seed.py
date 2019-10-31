812da6f7a143a4952d1e056220b958a65287638f
Robert Park <rober-park@hotmail.com>
2019-10-31 00:57:07 -0400

    updated seed file to include real ants.

--- seed.py
+++ seed.py
@@ -8,27 +8,29 @@ db = SqliteDatabase('main.db')
 db.connect()
 db.create_tables([Ants, Colony, Species, ForageEvents, Users])
 
-Species.insert({
-    "species_name": "Species A",
+Species.replace({
+    "id": 1,
+    "species_name": "Carpenter Ants",
     "hp_multiplier": 1.25,
-    "image_url": "https://i.imgur.com/8ICuNkT.png",
+    "image_url": "https://cdn.discordapp.com/attachments/197012817134616577/639316147891732491/JPEG_20190927_214519.jpg",
     "strength_multiplier": 1.0,
     "forage_multiplier": 0.75
 }).execute()
 
-Species.insert({
-    "species_name": "Species B",
+Species.replace({
+    "id": 2,
+    "species_name": "Yellow Crazy Ants",
     "hp_multiplier": 0.75,
-    "image_url": "https://i.imgur.com/eQw9QBk.png",
+    "image_url": "https://invasives.org.au/wp-content/uploads/2016/04/fs-yellow-crazy-ant.jpg",
     "strength_multiplier": 1.25,
     "forage_multiplier": 1.0
 }).execute()
 
-Species.insert({
-    "species_name": "Species C",
+Species.replace({
+    "id": 3,
+    "species_name": "Fire Ants",
     "hp_multiplier": 1.1,
-    "image_url": "https://i.imgur.com/fzb9xvm.png",
+    "image_url": "https://i.guim.co.uk/img/static/sys-images/Guardian/Pix/pictures/2014/12/8/1418073100561/44f3fbab-89c8-4a1f-95b5-b330cb6d8523-1020x612.jpeg?width=620&quality=85&auto=format&fit=max&s=ae46ffd7415131288c781d7985b0c2d8",
     "strength_multiplier": 1.1,
     "forage_multiplier": 1.1
 }).execute()
-
