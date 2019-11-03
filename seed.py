860f1c6fdeb94a16a7cfe53509bc5ad7af1735cb
Brian Le <brainicism@gmail.com>
2019-11-03 13:25:19 -0800

    Refactor init data to json files

--- seed.py
+++ seed.py
@@ -5,7 +5,8 @@ from models.users import Users
 from models.species import Species
 from models.forage_events import ForageEvents
 from models.shop_items import ShopItems
-from forage_event_data import *
+import json
+
 
 def seed():
     print("Seeding database...")
@@ -13,57 +14,42 @@ def seed():
     db.connect()
     db.create_tables([Ants, Colony, Species, ForageEvents, ShopItems, Users])
 
-    Species.replace({
-        "id": 1,
-        "species_name": "Carpenter Ants",
-        "hp_multiplier": 1.25,
-        "image_url": "https://cdn.discordapp.com/attachments/197012817134616577/639316147891732491/JPEG_20190927_214519.jpg",
-        "strength_multiplier": 1.0,
-        "forage_multiplier": 0.75
-    }).execute()
-    Species.replace({
-        "id": 2,
-        "species_name": "Yellow Crazy Ants",
-        "hp_multiplier": 0.75,
-        "image_url": "https://invasives.org.au/wp-content/uploads/2016/04/fs-yellow-crazy-ant.jpg",
-        "strength_multiplier": 1.25,
-        "forage_multiplier": 1.0
-    }).execute()
-    Species.replace({
-        "id": 3,
-        "species_name": "Fire Ants",
-        "hp_multiplier": 1.1,
-        "image_url": "https://i.guim.co.uk/img/static/sys-images/Guardian/Pix/pictures/2014/12/8/1418073100561/44f3fbab-89c8-4a1f-95b5-b330cb6d8523-1020x612.jpeg?width=620&quality=85&auto=format&fit=max&s=ae46ffd7415131288c781d7985b0c2d8",
-        "strength_multiplier": 1.1,
-        "forage_multiplier": 1.1
-    }).execute()
-
-    forage_event_id = 1
-    for event in FORAGE_EVENT_DATA:
-        ForageEvents.replace({
-            "id": forage_event_id,
-            "event_description": FORAGE_EVENT_DATA[event]["description"],
-            "event_rarity": FORAGE_EVENT_DATA[event]["rarity"],
-            "event_death_multiplier": FORAGE_EVENT_DATA[event]["death_multiplier"],
-            "event_food_multiplier": FORAGE_EVENT_DATA[event]["food_multiplier"],
-            "image_url": FORAGE_EVENT_DATA[event]["image_url"]
+    for species in parse_json_from_file("data/species.json"):
+        Species.replace({
+            "id": species["id"],
+            "species_name": species["species_name"],
+            "hp_multiplier": species["hp_multiplier"],
+            "image_url": species["image_url"],
+            "strength_multiplier": species["strength_multiplier"],
+            "forage_multiplier": species["forage_multiplier"]
         }).execute()
-        forage_event_id += 1
 
-    ShopItems.replace({
-        "id": 1,
-        "name": "Food Capacity",
-        "description": "",
-        "price": 40
-    }).execute()
-    ShopItems.replace({
-        "id": 2,
-        "name": "Ant Capacity",
-        "description": "",
-        "price": 50
-    }).execute()
+    for event in parse_json_from_file("data/forage_events.json"):
+        ForageEvents.replace({
+            "id": event["id"],
+            "event_description": event["description"],
+            "event_rarity": event["rarity"],
+            "event_death_multiplier": event["death_multiplier"],
+            "event_food_multiplier": event["food_multiplier"],
+            "image_url": event["image_url"]
+        }).execute()
+
+    for shop_item in parse_json_from_file("data/shop_items.json"):
+        ShopItems.replace({
+            "id": shop_item["id"],
+            "name": shop_item["name"],
+            "description": shop_item["description"],
+            "price": shop_item["price"]
+        }).execute()
 
     db.close()
 
+
 if __name__ == '__main__':
     seed()
+
+
+def parse_json_from_file(file_name):
+    with open(file_name) as json_file:
+        data = json.load(json_file)
+        return data
