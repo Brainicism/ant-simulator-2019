69a363d3b05b5d591cbd402ac4788e07a5e07dc7
Brian Le <brainicism@gmail.com>
2019-11-03 13:42:49 -0800

    Fix references to fk fields

--- event_loop.py
+++ event_loop.py
@@ -13,7 +13,7 @@ def hatch_egg_check(current_time):
     hatched_eggs = Ants.select().where((Ants.life_stage == 0) & (Ants.birth_date < current_time - datetime.timedelta(days=3)))
     colony_egg_count = defaultdict(int)
     for egg in hatched_eggs:
-        colony_egg_count[egg.colony_id.id] += 1
+        colony_egg_count[egg.colony.id] += 1
         egg.life_stage = 1
         egg.save()
     for colony_id in colony_egg_count:
