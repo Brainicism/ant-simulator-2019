4d0a9654437062954d86887d389b6bb17a14fc84
Brian Le <brainicism@gmail.com>
2019-11-03 13:47:07 -0800

    Add backrefs to fk, remove redundant to_fields

--- models/colony.py
+++ models/colony.py
@@ -4,8 +4,8 @@ from .species import Species
 from .base_model import BaseModel
 
 class Colony(BaseModel):
-    user_id = ForeignKeyField(Users, on_delete="CASCADE")
-    species_id = ForeignKeyField(Species, to_field="id")
+    user_id = ForeignKeyField(Users, on_delete="CASCADE", backref="colonies")
+    species_id = ForeignKeyField(Species, backref="colonies")
     colony_name = CharField()
     current_food_supply = IntegerField()
     max_food_supply = IntegerField()
